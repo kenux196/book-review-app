efb88421b0f879bc69c5928d1e8dbd4287585283
kenux196 <kenux.yun@gmail.com>
2025-12-01 21:38:25 +0900

    ci: add github action for tests and coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     environment: "happy-dom",
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "html", "json-summary"],
       exclude: [
         "node_modules/",
         "src/test-utils/",
